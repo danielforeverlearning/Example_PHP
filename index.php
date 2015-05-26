cffe9c4f50d1811877e10b7c0675d93fcbcbf524
danielforeverlearning <danielforeverlearning@gmail.com>
2015-05-25 14:24:14 -1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+User password: <input type="password" name="psw"><br>
+<br>
+<input type="radio" name="sex" value="male" checked>Male<br>
+<input type="radio" name="sex" value="female">Female<br>
+<br>
+<input type="checkbox" name="bikequestion" value="yes">I have a bike<br>
+<input type="checkbox" name="carquestion" value="yes">I have a car<br>
+<br>
+<select name="icecream">
+                <option value="double chocolate">Chocolate</option>
+                <option value="vanilla">Vanilla</option>
+                <option value="strawberry">Strawberry</option>
+                <option value="caramel">Caramel</option>
+</select>
+<br>
+<br>
+
+<input type="submit">
+</form>
+
+</body>
+</html>
